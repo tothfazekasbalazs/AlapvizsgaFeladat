14361726394b30a5f2a07eb5e43c750b2fe427b5
Tóth Fazekas Balázs <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2022-09-16 23:17:27 +0200

    Dícsőítés teljesen kész (Amiben a szemüveget dícsérem a nagy kép alatt)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
-function Bt1() {
-    window.open("index.html","_self"); 
+function bt1() { // function OpenMe_Facebook
+    window.open("https://amagyartermek.hu/magyar-termek#vedjegy-tipusok/hazai-termek"); // nyisd meg ezt az oldalt egy új oldalon
+    // egyelőre GOOGLE.COM a megnyitandó oldal, mert jelenlegi még nincs készen belinkelásre
 }
