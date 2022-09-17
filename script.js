ec44715dfbf6a1ef8b2aa00f41fd3e4e220431ba
Tóth Fazekas Balázs <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2022-09-17 18:16:35 +0200

    Mappa elrendezés és hoverezés

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function bt1() { // function OpenMe_Facebook
     window.open("https://amagyartermek.hu/magyar-termek#vedjegy-tipusok/hazai-termek"); // nyisd meg ezt az oldalt egy új oldalon
     // egyelőre GOOGLE.COM a megnyitandó oldal, mert jelenlegi még nincs készen belinkelásre
 }
+
+function bt2() { // function OpenMe_Facebook
+    window.open("index.html"); // nyisd meg ezt az oldalt egy új oldalon
+    // egyelőre GOOGLE.COM a megnyitandó oldal, mert jelenlegi még nincs készen belinkelásre
+}
