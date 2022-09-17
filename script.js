d952d9850f3d01b1c10e163862207261ce082220
Tóth Fazekas Balázs <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2022-09-17 20:48:49 +0200

    Nyitvatartás táblázat kész

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function bt1() { // function OpenMe_Facebook
 }
 
 function bt2() { // function OpenMe_Facebook
-    window.open("index.html"); // nyisd meg ezt az oldalt egy új oldalon
+    window.open("index.html","_self"); // nyisd meg ezt az oldalt egy új oldalon
     // egyelőre GOOGLE.COM a megnyitandó oldal, mert jelenlegi még nincs készen belinkelásre
 }
 
 function bt3() { // function OpenMe_Facebook
-    window.open(""); // nyisd meg ezt az oldalt egy új oldalon
+    window.open("nyitvat.html","_self"); // nyisd meg ezt az oldalt egy új oldalon
     // egyelőre GOOGLE.COM a megnyitandó oldal, mert jelenlegi még nincs készen belinkelásre
 }
