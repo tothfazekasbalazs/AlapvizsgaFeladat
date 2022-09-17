1087a886a87a03c66bf581dc55fbf4e67d14f3bf
Tóth Fazekas Balázs <toth-fazekas.balazs.2021i@bankitatabanya.hu>
2022-09-17 20:16:53 +0200

    Áraink és nyitvatartás kész

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ function bt2() { // function OpenMe_Facebook
     window.open("index.html"); // nyisd meg ezt az oldalt egy új oldalon
     // egyelőre GOOGLE.COM a megnyitandó oldal, mert jelenlegi még nincs készen belinkelásre
 }
+
+function bt3() { // function OpenMe_Facebook
+    window.open(""); // nyisd meg ezt az oldalt egy új oldalon
+    // egyelőre GOOGLE.COM a megnyitandó oldal, mert jelenlegi még nincs készen belinkelásre
+}
